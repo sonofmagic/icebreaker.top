156fbb26e7aa0cb58f3589502b9c50d6248cee53
SonOfMagic <qq1324318532@gmail.com>
2021-07-06 00:43:58 +0800

    feat: add a refresh dialog

--- packages/@som/blog/client/layouts/default.vue
+++ packages/@som/blog/client/layouts/default.vue
@@ -7,10 +7,36 @@
 </template>
 <script>
 import CommonSlot from '@/components/layout/CommonSlot'
+import { Dialog } from 'vant'
 export default {
   name: 'PublicLayout',
   components: {
     CommonSlot,
+  },
+  async mounted() {
+    // this.showUpgradeDialog()
+    const workbox = await window.$workbox
+    if (workbox) {
+      workbox.addEventListener('installed', (event) => {
+        // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
+        if (event.isUpdate) {
+          this.showUpgradeDialog()
+          // whatever logic you want to use to notify the user that they need to refresh the page.
+        }
+      })
+    }
+  },
+  methods: {
+    showUpgradeDialog() {
+      Dialog.alert({
+        title: '提示',
+        message: '检测到新的内容',
+        theme: 'round-button',
+        confirmButtonText: '立即更新',
+      }).finally(() => {
+        location.reload()
+      })
+    },
   },
 }
 </script>
