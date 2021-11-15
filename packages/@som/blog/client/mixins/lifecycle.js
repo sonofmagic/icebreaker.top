a8016042bf5472ac3b3ff6c0012481006e5e4c2c
SonOfMagic <qq1324318532@gmail.com>
2021-11-15 21:38:34 +0800

    chore: sync 20211115

---
+++ packages/@som/blog/client/mixins/lifecycle.js
@@ -0,0 +1,51 @@
+/**
+ *
+ * @param {string} componentName
+ * @returns
+ */
+import cloneDeep from 'lodash/cloneDeep'
+
+function getValue(instance) {
+  return Object.entries(instance).reduce((acc, [k, v]) => {
+    // if (cur[0][0] === '$') {
+    //   acc[cur[0]] = cur[1]
+    // }
+    acc[k] = v
+    return acc
+  }, {})
+}
+export function createLifecycleMxin(componentName) {
+  /**
+   * @type {import('vue/types/index').Component} lifecycleMxin
+   */
+  const lifecycleMxin = {
+    data() {
+      return {
+        cctv: true,
+      }
+    },
+    beforeCreate() {
+      if (process.client) {
+        console.log(`[${componentName}] beforeCreate`, getValue(this))
+      }
+    },
+    created() {
+      if (process.client) {
+        console.log(`[${componentName}] created`, getValue(this))
+      }
+    },
+    beforeMount() {
+      console.log(`[${componentName}] beforeMount`, getValue(this))
+    },
+    mounted() {
+      console.log(`[${componentName}] mounted`, getValue(this))
+    },
+    beforeDestroy() {
+      console.log(`[${componentName}] beforeDestroy`, getValue(this))
+    },
+    destroyed() {
+      console.log(`[${componentName}] destroyed`, getValue(this))
+    },
+  }
+  return lifecycleMxin
+}
