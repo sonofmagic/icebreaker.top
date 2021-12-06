cd0e82eff0b54de912b1dec0f0e2291276d4e02a
ice breaker <1324318532@qq.com>
2021-12-06 10:26:49 +0800

    feat: var-css commit

---
+++ packages/@som/blog/client/pages/test/css-var.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mx-auto pt-10 h-screen">
+    <div
+      class="h-32 w-32 flex justify-center items-center test-button"
+      @click="toggle"
+    >
+      测试
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    toggle() {
+      // const primaryColor =
+      //   document.documentElement.style.getPropertyValue('--primary-color')
+      // console.log(primaryColor)
+      this.count++
+      if (this.count % 2) {
+        document.documentElement.style.setProperty('--primary-color', 'green')
+      } else {
+        document.documentElement.style.setProperty('--primary-color', 'red')
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
