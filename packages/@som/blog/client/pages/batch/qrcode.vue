1d271dc94d8a97b6ce15a6d23925893dbfbeb808
ice breaker <1324318532@qq.com>
2021-12-06 13:52:44 +0800

    feat: test fro jszip

--- packages/@som/blog/client/pages/batch/qrcode.vue
+++ packages/@som/blog/client/pages/batch/qrcode.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto space-y-2">
     <div>route:{{ $route.query.v }}</div>
-    <canvas v-show="false" ref="canvasEl"></canvas>
+    <canvas v-show="!offscreen" ref="canvasEl"></canvas>
 
     <div>宏任务:{{ num }}</div>
 
@@ -17,7 +17,14 @@
 
         <div>
           <button
-            class="rounded bg-primary-600 text-white px-2 py-1 disabled:cursor-not-allowed disabled:opacity-75"
+            class="
+              rounded
+              bg-primary-600
+              text-white
+              px-2
+              py-1
+              disabled:cursor-not-allowed disabled:opacity-75
+            "
             :disabled="buttonDisabled"
             @click="download"
           >
@@ -33,6 +40,7 @@
           <div>生成个数:{{ count }}</div>
           <div>总耗时(ms):{{ costMs }}</div>
         </div>
+        <h2 class="text-2xl text-primary-900">STORE(no compression)</h2>
         <table class="border-collapse">
           <thead>
             <tr>
@@ -63,6 +71,71 @@
             </tr>
           </tbody>
         </table>
+
+        <h2 class="text-2xl text-primary-900">STORE(no compression)</h2>
+        <h3>11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz 1.69 GHz</h3>
+        <table class="border-collapse">
+          <thead>
+            <tr>
+              <th>参考个数</th>
+              <th>生成时间(ms)</th>
+              <th>压缩时间(ms)</th>
+              <th>总时间(ms)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="hover:bg-primary-200">
+              <td class="primary-td">10</td>
+              <td class="primary-td">166.50</td>
+              <td class="primary-td">6.70</td>
+              <td class="primary-td">173.20</td>
+            </tr>
+            <tr class="hover:bg-primary-200">
+              <td class="primary-td">100</td>
+              <td class="primary-td">1665.00</td>
+              <td class="primary-td">43.40</td>
+              <td class="primary-td">1708.40</td>
+            </tr>
+            <tr class="hover:bg-primary-200">
+              <td class="primary-td">1000</td>
+              <td class="primary-td">16706.50</td>
+              <td class="primary-td">358.10</td>
+              <td class="primary-td">17064.60</td>
+            </tr>
+          </tbody>
+        </table>
+        <h2 class="text-2xl text-primary-900">DEFLATE(9 level)</h2>
+        <h3>11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz 1.69 GHz</h3>
+        <table class="border-collapse">
+          <thead>
+            <tr>
+              <th>参考个数</th>
+              <th>生成时间(ms)</th>
+              <th>压缩时间(ms)</th>
+              <th>总时间(ms)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="hover:bg-primary-200">
+              <td class="primary-td">10</td>
+              <td class="primary-td">166.70</td>
+              <td class="primary-td">33.10</td>
+              <td class="primary-td">199.80</td>
+            </tr>
+            <tr class="hover:bg-primary-200">
+              <td class="primary-td">100</td>
+              <td class="primary-td">1662.60</td>
+              <td class="primary-td">142.20</td>
+              <td class="primary-td">1804.80</td>
+            </tr>
+            <tr class="hover:bg-primary-200">
+              <td class="primary-td">1000</td>
+              <td class="primary-td">16672.40</td>
+              <td class="primary-td">964.30</td>
+              <td class="primary-td">17636.70</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
@@ -72,7 +145,7 @@
 import { saveAs } from 'file-saver'
 import QRCode from 'qrcode'
 import JSZip from 'jszip'
-import Worker from '@/workers/zip.worker.js'
+
 // js主线程
 // 10个 197ms
 // 100 个 1819ms
@@ -89,16 +162,20 @@ export default {
       status: '', // 压缩中
       num: 0,
       buttonDisabled: false,
+      offscreen: true,
     }
   },
   mounted() {
     this.timer = setInterval(() => {
       this.num += 1
     }, 1000)
-    const worker = (this.worker = new Worker())
+    if (window.Worker) {
+      const Worker = require('@/workers/zip.worker.js').default
+      const worker = (this.worker = new Worker())
 
-    worker.onmessage = (event) => {
-      console.log(event)
+      worker.onmessage = (event) => {
+        console.log(event)
+      }
     }
   },
   beforeDestroy() {
@@ -124,6 +201,8 @@ export default {
             'https://www.icebreaker.top/batch/qrcode?v=' + i,
             {
               margin: 0,
+              errorCorrectionLevel: 'H',
+              // version:
             }
           )
           const filename = `qrcode${i}.png`
@@ -139,7 +218,13 @@ export default {
         this.status = '压缩中'
         const t1 = performance.now()
         const content = await zip.generateAsync(
-          { type: 'blob' },
+          {
+            type: 'blob',
+            compression: 'DEFLATE',
+            compressionOptions: {
+              level: 9,
+            },
+          },
           (metadata) => {
             // console.log('progression: ' + metadata.percent.toFixed(2) + ' %')
             this.percent = 50 + metadata.percent / 2
@@ -151,8 +236,13 @@ export default {
         saveAs(content, 'qrcodes.zip')
         this.status = '生成成功!'
         const t2 = performance.now()
-        this.costMs = t2 - t0
-        console.log(`生成时间:${t1 - t0},压缩时间:${t2 - t1}`)
+        const totalCostTime = (t2 - t0).toFixed(2)
+        console.log(
+          `生成时间:${(t1 - t0).toFixed(2)},压缩时间:${(t2 - t1).toFixed(
+            2
+          )},总耗时:${totalCostTime}`
+        )
+        this.costMs = totalCostTime
       } catch (error) {
         console.error(error)
       } finally {
