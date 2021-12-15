3bfdbd8162f9fbee90f4e60a5537ef4ca847d838
ice breaker <1324318532@qq.com>
2021-12-15 11:21:09 +0800

    feat: copy github vars to local

--- packages/@som/blog/client/pages/index.vue
+++ packages/@som/blog/client/pages/index.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="qwe"></div>
+  <div class="application-main flex min-h-screen">
+    <aside class="team-left-column w-4/12"></aside>
+    <div class="w-8/12"></div>
+  </div>
 </template>
 
 <script>
+export default {
+  layout: 'noFooter',
+}
 </script>
 
 <style lang="scss" scoped>
-.qwe {
-  color: $header-text;
+.application-main {
+  @apply bg-canvas-inset;
+  .team-left-column {
+    @apply bg-canvas-default max-w-full;
+  }
+  @screen md {
+    .team-left-column {
+      @apply max-w-[350px];
+    }
+  }
 }
 </style>
--- packages/@som/blog/client/pages/test/generator.vue
+++ packages/@som/blog/client/pages/test/generator.vue
@@ -2,12 +2,12 @@
   <div class="container mx-auto flex">
     <div class="w-1/2">
       <div v-for="(v, k) in variables" :key="k">
-        '{{ removeColorPrefix(k) }}':withOpacityValue('{{ k }}'),
+        '{{ removeColorPrefix(k) }}':{{ jsFilterShadow(k) }},
       </div>
     </div>
     <div class="w-1/2">
       <div v-for="(v, k) in variables" :key="k">
-        ${{ removeColorPrefix(k) }}:rgb(var({{ k }}));
+        ${{ removeColorPrefix(k) }}:{{ scssFilterShadow(k) }};
       </div>
     </div>
   </div>
@@ -15,15 +15,33 @@
 
 <script>
 import variables from '@/assets/scss/export.scss'
+import copyTextMixin from '@/mixins/copyTextMixin'
 export default {
+  mixins: [copyTextMixin],
   data() {
     return {
       variables,
     }
   },
+
   methods: {
     removeColorPrefix(str) {
+      // if (str.includes('shadow')) {
+      //   return str
+      // }
       return str.substring(8)
+    },
+    jsFilterShadow(str) {
+      if (str.includes('shadow')) {
+        return `'${str}'`
+      }
+      return `withOpacityValue('${str}')`
+    },
+    scssFilterShadow(str) {
+      if (str.includes('shadow')) {
+        return `var(${str})`
+      }
+      return `rgb(var(${str}))`
     },
   },
 }
