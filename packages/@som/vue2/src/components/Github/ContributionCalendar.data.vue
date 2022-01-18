772d6ba38b860dc1038d139e5ea6be01ebc6ad9c
ice breaker <1324318532@qq.com>
2022-01-18 14:21:53 +0800

    feat: commit compostion

--- packages/@som/vue2/src/components/Github/ContributionCalendar.data.vue
+++ packages/@som/vue2/src/components/Github/ContributionCalendar.data.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div ref="box"></div>
-    <div ref="tooltip" class="svg-tip svg-tip-one-line">
-      <div ref="tooltipContent"></div>
+    <div ref="boxEl"></div>
+    <div ref="tooltipEl" class="svg-tip svg-tip-one-line">
+      <div ref="tooltipContentEl"></div>
       <div class="arrow" data-popper-arrow></div>
     </div>
   </div>
@@ -10,16 +10,20 @@
 
 <script lang="ts">
 import * as d3 from 'd3'
-import Vue from 'vue'
 import { createPopper } from '@popperjs/core'
 import dayjs from 'dayjs'
+import { defineComponent, onMounted, ref } from '@vue/composition-api'
 
-export default Vue.extend({
-  methods: {
-    addPopper (targetEl: SVGRectElement) {
-      const tooltip = this.$refs.tooltip as HTMLDivElement
-      const tooltipContent = this.$refs.tooltipContent as HTMLDivElement
-      // const targetEl = this.$refs.box as HTMLDivElement
+export default defineComponent({
+  name: 'ContributionCalendar.data',
+  setup (props, ctx) {
+    const boxEl = ref<HTMLDivElement>()
+    const tooltipEl = ref<HTMLDivElement>()
+    const tooltipContentEl = ref<HTMLDivElement>()
+    function addPopper (targetEl: SVGRectElement) {
+      const tooltip = tooltipEl.value as HTMLDivElement
+      const tooltipContent = tooltipContentEl.value as HTMLDivElement
+
       const popperInstance = createPopper(targetEl, tooltip, {
         placement: 'top'
       })
@@ -51,123 +55,126 @@ export default Vue.extend({
 
       return popperInstance
     }
-  },
-  mounted () {
-    const width = 722
-    const height = 112
+    onMounted(() => {
+      const width = 722
+      const height = 112
 
-    const now = dayjs()
-    const vm = this
-    const colItemCount = 7
+      const now = dayjs()
+      const colItemCount = 7
 
-    const svg = d3
-      .select(this.$refs.box as HTMLDivElement)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-    const wrapper = svg.append('g').attr('transform', 'translate(10, 20)')
+      const svg = d3
+        .select(boxEl.value as HTMLDivElement)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+      const wrapper = svg.append('g').attr('transform', 'translate(10, 20)')
 
-    const zoomHandler = d3
-      .zoom<SVGSVGElement, unknown>()
-      .extent([
-        [0, 0],
-        [width, height]
-      ])
-      .scaleExtent([1, 8])
-      .on('zoom', function (attrs) {
-        console.log(attrs)
-        wrapper.attr('transform', attrs.transform)
-      })
-    svg.call(zoomHandler)
+      const zoomHandler = d3
+        .zoom<SVGSVGElement, unknown>()
+        .extent([
+          [0, 0],
+          [width, height]
+        ])
+        .scaleExtent([1, 8])
+        .on('zoom', function (attrs) {
+          // console.log(attrs)
+          wrapper.attr('transform', attrs.transform)
+        })
 
-    const matrix = []
-    let colIndex = -1
-    for (let i = 0; i < 365; i++) {
-      const yIdx = i % colItemCount
-      if (yIdx === 0) {
-        colIndex++
-        matrix.push([i])
-      } else {
-        matrix[colIndex].push(i)
+      svg.call(zoomHandler).on('wheel.zoom', null)
+
+      const matrix = []
+      let colIndex = -1
+      for (let i = 0; i < 365; i++) {
+        const yIdx = i % colItemCount
+        if (yIdx === 0) {
+          colIndex++
+          matrix.push([i])
+        } else {
+          matrix[colIndex].push(i)
+        }
       }
-    }
-    wrapper
-      .selectAll('g')
-      .data(matrix)
-      .join('g')
-      .attr('transform', function (d, i, g) {
-        return `translate(${i * 14}, 0)`
-      })
-      .selectAll('rect')
-      .data((d) => d)
-      .join('rect')
-      .text((d) => d)
-      .attr('width', 10)
-      .attr('height', 10)
-      .attr('x', 14)
-      .attr('y', function (d, i) {
-        return i * 13
-      })
-      .attr('rx', 2)
-      .attr('ry', 2)
-      .attr('data-level', function (d, i) {
-        return i % 4
-      })
-      .attr('data-count', function (d, i) {
-        return i
-      })
-      .attr('data-date', function (d, i) {
-        return now.add(i, 'd').format('YYYY-MM-DD')
-      })
-      .classed('ContributionCalendar-day', true)
-      .each(function (d, i, g) {
-        vm.addPopper(this as SVGRectElement)
-      })
+      wrapper
+        .selectAll('g')
+        .data(matrix)
+        .join('g')
+        .attr('transform', function (d, i, g) {
+          return `translate(${i * 14}, 0)`
+        })
+        .selectAll('rect')
+        .data((d) => d)
+        .join('rect')
+        .text((d) => d)
+        .attr('width', 10)
+        .attr('height', 10)
+        .attr('x', 14)
+        .attr('y', function (d, i) {
+          return i * 13
+        })
+        .attr('rx', 2)
+        .attr('ry', 2)
+        .attr('data-level', function (d, i) {
+          return i % 4
+        })
+        .attr('data-count', function (d, i) {
+          return i
+        })
+        .attr('data-date', function (d, i) {
+          return now.add(i, 'd').format('YYYY-MM-DD')
+        })
+        .classed('ContributionCalendar-day', true)
+        .each(function (d, i, g) {
+          addPopper(this as SVGRectElement)
+        })
 
-    function addXAxis () {
-      const xAxis = [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec'
-      ]
-      for (let i = 0; i < xAxis.length; i++) {
-        const label = xAxis[i]
-        wrapper
-          .append('text')
-          .classed('ContributionCalendar-label', true)
-          .attr('y', -7)
-          .attr('x', (i * 4 + 1) * 14)
-          .text(label)
+      function addXAxis () {
+        const xAxis = [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec'
+        ]
+        for (let i = 0; i < xAxis.length; i++) {
+          const label = xAxis[i]
+          wrapper
+            .append('text')
+            .classed('ContributionCalendar-label', true)
+            .attr('y', -7)
+            .attr('x', (i * 4 + 1) * 14)
+            .text(label)
+        }
       }
-    }
 
-    function addYAxis () {
-      const yAxis = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-      for (let i = 0; i < yAxis.length; i++) {
-        const label = yAxis[i]
-        wrapper
-          .append('text')
-          .classed('ContributionCalendar-label', true)
-          .attr('text-anchor', 'start')
-          .attr('x', '-10')
-          .attr('y', 8 + i * 13)
-          .text(label)
+      function addYAxis () {
+        const yAxis = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        for (let i = 0; i < yAxis.length; i++) {
+          const label = yAxis[i]
+          wrapper
+            .append('text')
+            .classed('ContributionCalendar-label', true)
+            .attr('text-anchor', 'start')
+            .attr('x', '-10')
+            .attr('y', 8 + i * 13)
+            .text(label)
+        }
       }
-    }
-    addXAxis()
-    addYAxis()
+      addXAxis()
+      addYAxis()
+    })
 
-    // https://popper.js.org/docs/v2/tutorial/#functionality
-    // const state = await popperInstance.update()
+    return {
+      boxEl,
+      tooltipEl,
+      tooltipContentEl
+    }
   }
 })
 </script>
