236904b7ff7905292267b0102d6c07ac950d88a4
SonOfMagic <qq1324318532@gmail.com>
2022-01-20 22:20:39 +0800

    feat: commit codes

--- packages/@som/vue2/src/components/Github/Demo.vue
+++ packages/@som/vue2/src/components/Github/Demo.vue
@@ -18,13 +18,12 @@
 import * as d3 from 'd3'
 import { createPopper } from '@popperjs/core'
 import dayjs from 'dayjs'
-import { defineComponent, onMounted, ref, watch } from '@vue/composition-api'
+import { defineComponent, onMounted, ref, onBeforeUnmount } from '@vue/composition-api'
 
 export default defineComponent({
   name: 'ContributionCalendar',
 
   setup (props, ctx) {
-    // const { dayCount } = toRefs(props)
     const now = dayjs()
     const startOfThisYear = now.startOf('y')
     const endOfThisYear = now.endOf('y')
@@ -32,23 +31,46 @@ export default defineComponent({
 
     const colItemCount = 7
 
-    watch(
-      props,
-      (nv) => {
-        console.log(nv)
-      },
-      {
-        deep: true
-      }
-    )
     const boxEl = ref<HTMLDivElement>()
     const tooltipEl = ref<HTMLDivElement>()
     const tooltipContentEl = ref<HTMLDivElement>()
     let svg: d3.Selection<SVGSVGElement, unknown, null, undefined>
-
+    let wrapper: d3.Selection<SVGGElement, unknown, null, undefined>
     const width = Math.floor(dayCount / colItemCount + 1) * 14 + 24
     const height = 112
 
+    type MatrixItem = {
+      no: number
+      level: number
+    }
+    const matrix: MatrixItem[][] = []
+    const icebreakerMatrix = [
+      [0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    let colIndex = -1
+    for (let i = 0; i < dayCount; i++) {
+      const yIdx = i % colItemCount
+      if (yIdx === 0) {
+        colIndex++
+        matrix.push([
+          {
+            no: i,
+            level: icebreakerMatrix[yIdx][colIndex] || 0
+          }
+        ])
+      } else {
+        matrix[colIndex].push({
+          no: i,
+          level: icebreakerMatrix[yIdx][colIndex] || 0
+        })
+      }
+    }
     function addPopper (targetEl: SVGRectElement) {
       const tooltip = tooltipEl.value as HTMLDivElement
       const tooltipContent = tooltipContentEl.value as HTMLDivElement
@@ -83,28 +105,32 @@ export default defineComponent({
       return popperInstance
     }
 
-    function render () {
-      svg = d3
-        .select(boxEl.value as HTMLDivElement)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height)
-      const wrapper = svg.append('g').attr('transform', 'translate(10, 20)')
-
-      const matrix = []
-      let colIndex = -1
-      for (let i = 0; i < dayCount; i++) {
-        const yIdx = i % colItemCount
-        if (yIdx === 0) {
-          colIndex++
-          matrix.push([i])
-        } else {
-          matrix[colIndex].push(i)
-        }
+    function prerender () {
+      if (!svg) {
+        svg = d3
+          .select(boxEl.value as HTMLDivElement)
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height)
+        wrapper = svg.append('g').attr('transform', 'translate(10, 20)')
       }
+    }
+
+    function animated (seed = 0) {
+      wrapper
+        .selectAll<SVGGElement, MatrixItem>('rect')
+        .filter((d) => Boolean(d.level))
+        .attr('data-level', function (d, i) {
+          // console.log(d)
+          return (d.level + seed) % 4
+        })
+    }
+    function render (data: MatrixItem[][]) {
+      prerender()
+
       wrapper
         .selectAll('g')
-        .data(matrix)
+        .data(data)
         .join('g')
         .attr('transform', function (d, i, g) {
           return `translate(${(i + 1) * 14}, 0)`
@@ -112,7 +138,7 @@ export default defineComponent({
         .selectAll('rect')
         .data((d) => d)
         .join('rect')
-        .text((d) => d)
+        .text((d) => d.no)
         .attr('width', 10)
         .attr('height', 10)
         .attr('x', 0)
@@ -121,35 +147,23 @@ export default defineComponent({
         })
         .attr('rx', 2)
         .attr('ry', 2)
-        .attr('data-level', function (d, i) {
-          return i % 4
-        })
+
         .attr('data-count', function (d, i) {
-          return d
+          return d.no
         })
         .attr('data-date', function (d, i) {
-          return startOfThisYear.add(d, 'd').format('YYYY-MM-DD')
+          return startOfThisYear.add(d.no, 'd').format('YYYY-MM-DD')
         })
         .classed('ContributionCalendar-day', true)
         .each(function (d, i, g) {
           addPopper(this as SVGRectElement)
         })
+        .attr('data-level', function (d, i) {
+          return d.level
+        })
 
       function addXAxis () {
-        const xAxis = [
-          'Jan',
-          'Feb',
-          'Mar',
-          'Apr',
-          'May',
-          'Jun',
-          'Jul',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec'
-        ]
+        const xAxis = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         for (let idx = 0; idx < xAxis.length; idx++) {
           const label = xAxis[idx]
           wrapper
@@ -157,9 +171,7 @@ export default defineComponent({
             .classed('ContributionCalendar-label', true)
             .attr('y', -7)
             .attr('x', function (d, i) {
-              const interval = startOfThisYear
-                .add(idx, 'M')
-                .diff(startOfThisYear, 'd')
+              const interval = startOfThisYear.add(idx, 'M').diff(startOfThisYear, 'd')
 
               const x = Math.floor(interval / 7)
               return (x + 1) * 14
@@ -187,8 +199,23 @@ export default defineComponent({
       addXAxis()
       addYAxis()
     }
+    let timer: number
     onMounted(() => {
-      render()
+      let seed = 0
+      render(matrix)
+      timer = setInterval(() => {
+        animated(seed)
+        seed++
+      }, 150)
+      // function step () {
+      //   animated(seed)
+      //   seed++
+      //   window.requestAnimationFrame(step)
+      // }
+      // window.requestAnimationFrame(step)
+    })
+    onBeforeUnmount(() => {
+      clearInterval(timer)
     })
 
     return {
