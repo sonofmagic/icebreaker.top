44bdb92a54253b5340e706ef7be9785edc9ccff6
SonOfMagic <qq1324318532@gmail.com>
2022-03-24 17:35:44 +0800

    feat: commit codes

---
+++ packages/@som/blog/prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('prettier').Config}
+ */
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
+
+module.exports = config
