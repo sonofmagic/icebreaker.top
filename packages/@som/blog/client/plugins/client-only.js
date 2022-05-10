2c9ccc17bb2e14c8965513ffe09b919a1829f107
SonOfMagic <1324318532@qq.com>
2022-05-10 19:08:08 +0800

    feat: remove aegis-web-sdk

--- packages/@som/blog/client/plugins/client-only.js
+++ packages/@som/blog/client/plugins/client-only.js
@@ -2,7 +2,7 @@
 
 import svg4everybody from 'svg4everybody'
 import mobile from 'is-mobile'
-import Aegis from 'aegis-web-sdk'
+// import Aegis from 'aegis-web-sdk'
 import { LocalStorageKey } from '@/enum/user'
 svg4everybody()
 
@@ -14,16 +14,16 @@ export default (ctx, inject) => {
   const theme = localStorage.getItem(LocalStorageKey.ThemeMode) || 'dark'
 
   ctx.store.commit('set_theme', theme)
-  const aegis = new Aegis({
-    id: 'Kb5Z6VMPJxGPAozvey', // 上报 id
-    // uin: 'xxx', // 用户唯一 ID（可选）
-    reportApiSpeed: true, // 接口测速
-    reportAssetSpeed: true, // 静态资源测速
-    spa: true, // spa 应用页面跳转的时候开启 pv 计算
-  })
+  // const aegis = new Aegis({
+  //   id: 'Kb5Z6VMPJxGPAozvey', // 上报 id
+  //   // uin: 'xxx', // 用户唯一 ID（可选）
+  //   reportApiSpeed: true, // 接口测速
+  //   reportAssetSpeed: true, // 静态资源测速
+  //   spa: true, // spa 应用页面跳转的时候开启 pv 计算
+  // })
 
   const isMobile = mobile()
   console.log(`isMobile:${isMobile}`)
   ctx.store.dispatch('device/setIsMobile', isMobile)
-  inject('aegis', aegis)
+  // inject('aegis', aegis)
 }
