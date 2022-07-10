e7b68c43b98a2b88ea85d710a3823b2f4e168258
SonOfMagic <1324318532@qq.com>
2022-07-11 01:16:03 +0800

    sync: 20220711

--- apps/blog/plugins/fortawesome.ts
+++ apps/blog/plugins/fortawesome.ts
@@ -1,6 +1,9 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+
+config.autoAddCss = false
+
 export default defineNuxtPlugin((nuxtApp) => {
   library.add(faSun, faMoon)
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
