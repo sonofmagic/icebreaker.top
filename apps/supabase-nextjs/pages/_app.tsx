bcd4138990c9bb22e0ff9c94f0bedf4dd220c16b
SonOfMagic <qq1324318532@gmail.com>
2022-10-09 16:58:59 +0800

    chore: init supabase nextjs

---
+++ apps/supabase-nextjs/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { UserProvider } from '@supabase/auth-helpers-react'
+import { supabaseClient } from '@supabase/auth-helpers-nextjs'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider supabaseClient={supabaseClient}>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
+}
+
+export default MyApp
