3a0dd4d83278e158c0eb54a65e2336faf29070b7
sonofmagic <1324318532@qq.com>
2022-10-10 21:53:33 +0800

    feat: commit codes

---
+++ apps/supabase-nextjs/pages/api/account/[[...routes]].ts
@@ -0,0 +1,47 @@
+// import { supabaseServerClient, withApiAuth, getProviderToken, getUser } from "@supabase/auth-helpers-nextjs";
+import { createClient } from '@supabase/supabase-js'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const {
+  NEXT_PUBLIC_SUPABASE_URL,
+  // NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  SUPABASE_SERVICE_ROLE_KEY,
+} = process.env as Record<string, string>
+const supabase = createClient(
+  NEXT_PUBLIC_SUPABASE_URL,
+  SUPABASE_SERVICE_ROLE_KEY
+)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { routes } = req.query as {
+    routes: string[]
+    [key: string]: string | string[]
+  }
+  const body = req.body
+  const route = routes[0]
+  const action = routes[1]
+  switch (route) {
+    case 'update': {
+      switch (action) {
+        case 'email': {
+          const { id, email } = body as { id: string; email: string }
+
+          const result = await supabase.auth.api.updateUser(id, {
+            email,
+          })
+          // Invalid token: token contains an invalid number of segments
+          if (result.error) {
+            res.status(result.error.status).send(result.error?.message)
+          } else {
+            res.status(200).send(result)
+          }
+
+          break
+        }
+      }
+    }
+  }
+}
--- apps/supabase-nextjs/pages/index.tsx
+++ apps/supabase-nextjs/pages/index.tsx
@@ -2,14 +2,22 @@ import { Auth } from '@supabase/ui'
 import { useUser } from '@supabase/auth-helpers-react'
 import { supabaseClient } from '@supabase/auth-helpers-nextjs'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
+interface TestRow {
+  id: number
+}
 
 const LoginPage = () => {
   const { user, error } = useUser()
-  const [data, setData] = useState()
+  const [data, setData] = useState<TestRow | null>(null)
 
   useEffect(() => {
     async function loadData() {
-      const { data } = await supabaseClient.from('test').select('*')
+      const { data } = await supabaseClient
+        .from<TestRow>('test')
+        .select('*')
+        .eq('id', 1)
+        .single()
       setData(data)
     }
     // Only run query once user is logged in.
@@ -32,6 +40,15 @@ const LoginPage = () => {
   return (
     <>
       <button onClick={() => supabaseClient.auth.signOut()}>Sign out</button>
+      <button
+        onClick={() => {
+          axios.post('/account/update/email', {
+            id: user.id,
+            email: '1324318532@qq.com',
+          })
+        }}>
+        Update Email
+      </button>
       <p>user:</p>
       <pre>{JSON.stringify(user, null, 2)}</pre>
       <p>client-side data fetching with RLS</p>
