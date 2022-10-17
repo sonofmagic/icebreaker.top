d74bbc02d78124b1c729c678984dc201e7b73b07
sonofmagic <1324318532@qq.com>
2022-10-18 00:20:04 +0800

    feat: commit codes

--- apps/supabase-vue-3/src/pages/storage.vue
+++ apps/supabase-vue-3/src/pages/storage.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-    <SupabaseUpload v-model:file-list="fileList">
-      <template #trigger>
-        <el-button type="primary">Click to upload</el-button>
-      </template>
-    </SupabaseUpload>
+  <div class="container mx-auto">
+    <div>
+      <div>pub bucket</div>
+      <SupabaseUpload bucket="pub" v-model:file-list="pubFileList">
+        <template #trigger>
+          <el-button type="primary">pub upload</el-button>
+        </template>
+      </SupabaseUpload>
+    </div>
+
+    <div>
+      <div>self bucket</div>
+      <SupabaseUpload bucket="self" v-model:file-list="selfFileList">
+        <template #trigger>
+          <el-button type="primary">self upload</el-button>
+        </template>
+      </SupabaseUpload>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { UploadProps, UploadUserFile } from 'element-plus'
-const fileList = ref<UploadUserFile[]>([])
+const pubFileList = ref<UploadUserFile[]>([])
+const selfFileList = ref<UploadUserFile[]>([])
 </script>
 
 <style scoped></style>
