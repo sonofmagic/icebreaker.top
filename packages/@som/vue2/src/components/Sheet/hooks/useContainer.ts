8c59963175a5227ee3b18be805c440a84a3c7e20
sonofmagic <1324318532@qq.com>
2022-10-25 23:59:31 +0800

    feat: build lib script

--- packages/@som/vue2/src/components/Sheet/hooks/useContainer.ts
+++ packages/@som/vue2/src/components/Sheet/hooks/useContainer.ts
@@ -1,6 +1,6 @@
 import { useElementBounding, MaybeElement, useScroll } from '@vueuse/core'
 import type { Ref } from 'vue-demi'
-export default function useContainer (el: Ref<HTMLElement | null | undefined>) {
+export default function useContainer(el: Ref<HTMLElement | null | undefined>) {
   const { x, y, top, right, bottom, left, width, height, update } = useElementBounding(el)
   const { x: scrollX, y: scrollY, directions, isScrolling, arrivedState } = useScroll(el)
   return {
--- packages/@som/vue2/vite.config.ts
+++ packages/@som/vue2/vite.config.ts
@@ -1,10 +1,15 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+// import vue from '@vitejs/plugin-vue'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+import { createVuePlugin } from 'vite-plugin-vue2'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    createVuePlugin({
+      jsx: true
+    })
+  ],
   optimizeDeps: {
     exclude: ['vue-demi']
   },
@@ -15,7 +20,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/components/vue-dom-sheet/index.vue'),
+      entry: resolve(__dirname, 'src/components/Sheet/index.vue'),
       name: 'vue-dom-sheet',
       // the proper extensions will be added
       fileName: 'vue-dom-sheet'
@@ -25,6 +30,7 @@ export default defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
+        dir: resolve(__dirname, 'src/components/Sheet/dist'),
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue'
