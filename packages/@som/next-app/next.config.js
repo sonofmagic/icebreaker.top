8ea5d432eeb825c3e3ab919b00a924b82f7c3de0
sonofmagic <1324318532@qq.com>
2022-11-09 21:14:41 +0800

    feat: commit codes

--- packages/@som/next-app/next.config.js
+++ packages/@som/next-app/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // load worker files as a urls with `file-loader`
+    config.module.rules.unshift({
+      test: /pdf\.worker\.(min\.)?js/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[contenthash].[ext]",
+            publicPath: "_next/static/worker",
+            outputPath: "static/worker"
+          }
+        }
+      ]
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
