e0ff16aab0ab7151ce94a2ec198fc3115fa3fb06
SonOfMagic <qq1324318532@gmail.com>
2023-03-23 13:49:27 +0800

    feat: commit codes

--- apps/blog-new/nuxt.config.ts
+++ apps/blog-new/nuxt.config.ts
@@ -1,2 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+  modules: [
+    // [
+    //   '@nuxtjs/eslint-module',
+    //   {
+    //     /* module options */
+    //   },
+    // ],
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxt/content',
+  ],
+  // devServer: {
+  //   host: '',
+  //   port: 3000,
+  // },
+})
---
+++ apps/blog-new/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
