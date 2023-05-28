ffa0abfde3573ebd95c27eb1bee946d48d650e67
sonofmagic <1324318532@qq.com>
2023-05-28 19:23:45 +0800

    chore: upgrade deps

--- apps/supabase-vue-3/src/main.ts
+++ apps/supabase-vue-3/src/main.ts
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import '@/assets/scss/global.scss'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from './router'
 import '@/plugins/fortawesome'
-const store = createPinia()
-
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
-app.use(store).use(router)
+app.use(pinia).use(router)
 app.mount('#app')
