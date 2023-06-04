cdb840c6cf05d2d7c398724a032a1b96947beb28
ice breaker <1324318532@qq.com>
2023-06-04 13:37:46 +0800

    feat: add more codes

--- apps/blog-new/components/AppNavbar.vue
+++ apps/blog-new/components/AppNavbar.vue
@@ -7,6 +7,10 @@ const appConfig = useAppConfig()
   <div class="flex justify-between max-w-2xl px-4 py-4 mx-auto sm:px-8">
     <!-- Navigation -->
     <div class="text-primary-700 dark:text-primary-200">
+      <NuxtLink to="/">
+        <TypingEffect></TypingEffect>
+      </NuxtLink>
+
       <!-- <NuxtLink
         v-for="link of navigation"
         :key="link._path"
@@ -19,9 +23,18 @@ const appConfig = useAppConfig()
     </div>
     <!-- Social icons & Color Mode -->
     <div class="space-x-3 transition text-primary-500">
-      <a v-if="appConfig.socials?.twitter" :href="`https://twitter.com/${appConfig.socials?.twitter}`" title="Twitter" class="dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300"><Icon name="fa-brands:twitter" class="w-5 h-5" /></a>
-      <a v-if="appConfig.socials?.npm" target="_blank" :href="`https://www.npmjs.com/~${appConfig.socials?.npm}`" title="npm" class="dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300"><Icon name="fa-brands:npm" class="w-5 h-5" /></a>
-      <a v-if="appConfig.socials?.github" target="_blank" :href="`https://github.com/${appConfig.socials?.github}`" title="GitHub" class="dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300"><Icon name="fa-brands:github" class="w-5 h-5" /></a>
+      <a v-if="appConfig.socials?.twitter" :href="`https://twitter.com/${appConfig.socials?.twitter}`" title="Twitter"
+        class="dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300">
+        <Icon name="fa-brands:twitter" class="w-5 h-5" />
+      </a>
+      <a v-if="appConfig.socials?.npm" target="_blank" :href="`https://www.npmjs.com/~${appConfig.socials?.npm}`"
+        title="npm" class="dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300">
+        <Icon name="fa-brands:npm" class="w-5 h-5" />
+      </a>
+      <a v-if="appConfig.socials?.github" target="_blank" :href="`https://github.com/${appConfig.socials?.github}`"
+        title="GitHub" class="dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300">
+        <Icon name="fa-brands:github" class="w-5 h-5" />
+      </a>
       <ColorModeSwitch class="dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300" />
     </div>
   </div>
