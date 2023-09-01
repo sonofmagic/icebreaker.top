6c8e6e4d73952938980da7119b994cdabc8fe462
ice breaker <1324318532@qq.com>
2023-09-01 15:43:14 +0800

    fix: 页脚 Powered by 取值过期 #8

--- packages/@som/blog/client/components/layout/Footer.vue
+++ packages/@som/blog/client/components/layout/Footer.vue
@@ -1,32 +1,26 @@
 <template>
   <section class="container mx-auto py-2">
     <div
-      class="flex flex-col sm:flex-row justify-between items-center px-4 text-gray-600 text-xs dark:text-accents-600"
-    >
+      class="dark:text-accents-600 flex flex-col items-center justify-between px-4 text-xs text-gray-600 sm:flex-row">
       <div>
         <span class="mr-4">© {{ fullYear }} icebreaker</span>
-        <a target="_blank" rel="nofollow" href="http://beian.miit.gov.cn"
-          >苏ICP备19002675号-2</a
-        >
+        <a target="_blank" rel="nofollow" href="http://beian.miit.gov.cn">
+          苏ICP备19002675号-2
+        </a>
       </div>
       <div>version:{{ version }}</div>
       <div>
         <nuxt-link class="mr-5" to="/sitemap">站点地图</nuxt-link>
         <OutSideLink
-          class="text-base hover:text-gray-700 mr-5"
+          class="mr-5 text-base hover:text-gray-700"
           raw
-          href="https://github.com/sonofmagic/icebreaker.top"
-        >
+          href="https://github.com/sonofmagic/icebreaker.top">
           <FontAwesomeIcon :icon="['fab', 'github']" />
         </OutSideLink>
-        Powered by
-        <OutSideLink href="https://docs.cloudbase.net/">
-          CloudBase
-        </OutSideLink>
-        <span class="mx-1">&</span>
-        <OutSideLink href="https://www.serverless.com/">
-          Serverless
-        </OutSideLink>
+        <!-- Powered by -->
+        <!-- <OutSideLink href="https://docs.cloudbase.net/">CloudBase</OutSideLink> -->
+        <!-- <span class="mx-1">&</span> -->
+        <!-- <OutSideLink href="https://www.serverless.com/">Serverless</OutSideLink> -->
       </div>
     </div>
   </section>
--- packages/@som/blog/client/components/layout/SidebarFooter.vue
+++ packages/@som/blog/client/components/layout/SidebarFooter.vue
@@ -1,34 +1,31 @@
 <template>
   <section class="container mx-auto py-2">
     <div
-      class="dark:text-accents-600 flex flex-col items-center justify-between px-4 text-xs text-gray-600 sm:flex-row"
-    >
+      class="dark:text-accents-600 flex flex-col items-center justify-between px-4 text-xs text-gray-600 sm:flex-row">
       <div>
         <span class="mr-4">© {{ fullYear }} icebreaker</span>
-        <a target="_blank" rel="nofollow" href="http://beian.miit.gov.cn"
-          >苏ICP备19002675号-2</a
-        >
+        <a target="_blank" rel="nofollow" href="http://beian.miit.gov.cn">
+          苏ICP备19002675号-2
+        </a>
       </div>
       <div>version:{{ version }}</div>
       <div class="flex flex-col items-center md:flex-row">
-        <div class="mr-3 space-x-3">
+        <div class="space-x-3">
           <nuxt-link to="/sitemap">站点地图</nuxt-link>
           <OutSideLink
             class="text-base hover:text-gray-700"
             raw
-            href="https://www.icebreaker.top/rss.xml"
-          >
+            href="https://www.icebreaker.top/rss.xml">
             <FontAwesomeIcon :icon="['fas', 'square-rss']" />
           </OutSideLink>
           <OutSideLink
             class="text-base hover:text-gray-700"
             raw
-            href="https://github.com/sonofmagic/icebreaker.top"
-          >
+            href="https://github.com/sonofmagic/icebreaker.top">
             <FontAwesomeIcon :icon="['fab', 'github']" />
           </OutSideLink>
         </div>
-        <div>
+        <!-- <div>
           Powered by
           <OutSideLink href="https://docs.cloudbase.net/">
             CloudBase
@@ -37,7 +34,7 @@
           <OutSideLink href="https://www.serverless.com/">
             Serverless
           </OutSideLink>
-        </div>
+        </div> -->
       </div>
     </div>
   </section>
