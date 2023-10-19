44f228d6739fd180d059fe305f4b54215203b303
ice breaker <1324318532@qq.com>
2023-10-20 01:25:05 +0800

    chore: add test case

---
+++ packages/@som/blog/test/postcss.test.ts
@@ -0,0 +1,38 @@
+import postcss from 'postcss'
+import type { Config } from 'tailwindcss'
+
+describe('postcss', () => {
+  it('rpx case 0', async () => {
+    const config: Config = {
+      content: [
+        'p-[0.32rpx]',
+        'm-[23.43rpx]',
+        'space-y-[12.0rpx]',
+        'w-[12rpx]',
+        'min-w-[12rpx]',
+        'max-w-[12rpx]',
+        'h-[12rpx]',
+        'min-h-[12rpx]',
+        'max-h-[12rpx]',
+        'basis-[32rpx]',
+        'text-[32rpx]'
+      ].map((x) => {
+        return {
+          raw: x
+        }
+      }),
+      corePlugins: {
+        preflight: false
+      }
+    }
+    const { css } = await postcss([
+      require('tailwindcss')({
+        config
+      })
+    ]).process('@tailwind components;@tailwind utilities;', {
+      from: undefined
+    })
+
+    expect(css).toMatchSnapshot()
+  })
+});
