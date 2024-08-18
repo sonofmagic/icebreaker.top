68d642ed0b0d0dea3f1d399458dc985793d1e1a0
ice breaker <1324318532@qq.com>
2024-08-18 18:33:53 +0800

    chore: transform project

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import { icebreaker } from '@icebreakers/eslint-config'
 export default icebreaker(
   {},
   {
-    ignores: ['**/fixtures/**'],
+    ignores: [
+      '**/fixtures/**',
+      'blog-archived/statistics',
+    ],
   },
 )
