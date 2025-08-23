f706baebd4ca6ff5b5d9a409df75c8102d33e275
ice breaker <1324318532@qq.com>
2025-08-23 18:15:07 +0800

    chore: x

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,15 @@ import { icebreaker } from '@icebreakers/eslint-config'
 
 export default icebreaker(
   {
-    vue: {
-      vueVersion: 2,
-      sfcBlocks: {
-        blocks: {
-          styles: false,
-        },
-      },
-    },
-    tailwindcss: true,
+    // vue: {
+    //   vueVersion: 2,
+    //   sfcBlocks: {
+    //     blocks: {
+    //       styles: false,
+    //     },
+    //   },
+    // },
+    // tailwindcss: true,
     ignores: ['**/fixtures/**'],
   },
 )
